ad06f7f4aca570744bdd88da36b36320e47ac0ee
Alexandr <bolshakovalex130@gmail.com>
2018-04-01 15:32:13 +0300

    Update Car.java
    fix Errors

--- src/main/java/ua/org/oa/Car.java
+++ src/main/java/ua/org/oa/Car.java
@@ -1,18 +1,40 @@
 package ua.org.oa;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import java.util.Objects;
-
-@AllArgsConstructor
-@Getter
-@Setter
 public class Car {
     private String mark;
     private String Model;
     private int ProdYear;
 
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getModel() {
+        return Model;
+    }
+
+    public void setModel(String model) {
+        Model = model;
+    }
+
+    public int getProdYear() {
+        return ProdYear;
+    }
+
+    public void setProdYear(int prodYear) {
+        ProdYear = prodYear;
+    }
+
+    public Car(String mark, String model, int prodYear) {
+
+        this.mark = mark;
+        Model = model;
+        ProdYear = prodYear;
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -38,16 +60,16 @@ public class Car {
 
     public static void main(String[] args) {
         Car dif = new Car("Porche", "911", 2018);
-        Car Petroll = new Car("Porche", "911", 2018){
+        Car Petroll = new Car("Porche", "911", 2018) {
             @Override
             public String toString() {
                 return "$classname{" +
-                        "mark='" + mark + '\'' +
-                        ", Model='" + Model + '\'' +
-                        ", ProdYear=" + ProdYear +
+                        "mark='" + getMark() + '\'' +
+                        ", Model='" + getModel() + '\'' +
+                        ", ProdYear=" + getProdYear() +
                         '}';
             }
-        }
-
+        };
+        System.out.println(Petroll.toString());
     }
 }
