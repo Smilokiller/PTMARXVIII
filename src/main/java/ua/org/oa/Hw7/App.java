bbec48185beba7fa86ca9e858953a2823d696846
smilo <smilokiller@mail.ru>
2018-06-18 15:57:52 +0300

    Hw7

--- src/main/java/ua/org/oa/Hw7/App.java
+++ src/main/java/ua/org/oa/Hw7/App.java
@@ -6,23 +6,23 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        WorkWithTreads wwt =new WorkWithTreads();
+        WorkWithTreads wwt = new WorkWithTreads();
         wwt.start();
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNext()){
-            if (sc.nextLine()!=null) {
+        while (sc.hasNext()) {
+            if (sc.nextLine() != null) {
                 System.out.println("STOP Tread Time Printer");
                 wwt.Stop();
                 break;
             }
         }
 
-        WorkWithRunable wwr=new WorkWithRunable();
+        WorkWithRunable wwr = new WorkWithRunable();
 
-        while (sc.hasNext()){
-            if (sc.nextLine()!=null) {
+        while (sc.hasNext()) {
+            if (sc.nextLine() != null) {
                 System.out.println("STOP Runable Time printer");
                 wwr.getTh().stop();
                 break;
@@ -30,5 +30,8 @@ public class App {
         }
         System.out.println("STOP main");
 
+
     }
-}
+    }
+
+
--- src/main/java/ua/org/oa/Pz5/Student/Group.java
+++ src/main/java/ua/org/oa/Pz5/Student/Group.java
@@ -54,5 +54,5 @@ public class Group implements Serializable{
                 ", StudentList=" + StudentList +
                 '}';
     }
-    
+
 }
